c4347fb6c374719da1454ee09a6e61360b0a8532
JustFiesta <mateusz.bocak9@gmail.com>
2024-03-19 12:31:38 +0100

    Sample curl test http reqest for given files

---
+++ assesments/ex2/helper curl.sh
@@ -0,0 +1,5 @@
+curl -X POST \
+  https://api.surveymonkey.com/v3/surveys \
+  -H 'Authorization: Bearer {Q-O3EUOJIMKUmVzhRvJXbGNpv.5KkmPHYWc7mvAv0hsMs2nhNbFWQrppLNSDc9AuDvoUVNVGvtKrH1vcYhbweW8WoA0hceOHUs2GDSGMWAiBlC44KqYGIyliqvgyWZ4u}' \
+  -H 'Content-Type: application/json' \
+  -d @correct_survey.json
