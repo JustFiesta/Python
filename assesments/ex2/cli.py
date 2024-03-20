54130e0534b9244fc1c6c6228ff4738b670b7917
JustFiesta <mateusz.bocak9@gmail.com>
2024-03-20 09:48:01 +0100

    try block for create parser, with cusom exception

--- assesments/ex2/cli.py
+++ assesments/ex2/cli.py
@@ -7,9 +7,24 @@ from dotenv import load_dotenv
 
 import api_communication
 
+
+class TooFewArgumentsException(Exception):
+    """Exception for too few arguments passed"""
+    def __init__(self, message):
+        self.message = "Please specify required file parameters: survey mails"
+        super().__init__(self.message)
+
+"""
+Custom parser subclass, throwing ArgumentParserError on too few arguments
+"""
+class ThrowingArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        raise TooFewArgumentsException(message)
+
+
 # create cli parser with arguments
 def create_parser():
-    parser = argparse.ArgumentParser(description="""Post surveys on surveymonkey.com""")
+    parser = ThrowingArgumentParser(description="""Post surveys on surveymonkey.com""")
     parser.add_argument("survey",
                         help="JSON file containing survey information")
     parser.add_argument("mails", 
@@ -20,32 +35,37 @@ def create_parser():
 # run function
 def main():
 
-    parser = create_parser()
-    args = parser.parse_args()
+    try:
+        parser = create_parser()
+        args = parser.parse_args()
+    except TooFewArgumentsException:
+        print("Please provided reqiured parameters!\n")
 
-    # check if any arguments were passed - if not display help
-    if all(value is None for value in vars(args).values()):
         parser.print_help()
         sys.exit(0)
-    else:
+    # check if any arguments were passed - if not display help
+    # if all(value is None for value in vars(args).values()):
+    #     parser.print_help()
+    #     sys.exit(0)
+    # else:
 
-        # both are required by parser (as positionals on default), so no need to exit if they are not present
-        if args.survey:
-            survey_json_file = str(args.survey)
-        if args.mails:
-            email_list_file = str(args.mails)
+    # both are required by parser (as positionals on default), so no need to exit if they are not present
+    if args.survey:
+        survey_json_file = str(args.survey)
+    if args.mails:
+        email_list_file = str(args.mails)
 
-        try:
-             # try to load api token from evnrioment variable
-            load_dotenv()
-            api_token = os.environ['SURVEY_MONKEY_API']
+    try:
+            # try to load api token from evnrioment variable
+        load_dotenv()
+        api_token = os.environ['SURVEY_MONKEY_API']
 
-            api_communication.post_survey(api_token, survey_json_file)
-            api_communication.post_survey_invitations(api_token, api_communication.survey_id, email_list_file)
-        
-        except api_communication.APIConnectionException as e:
-            print(e)
-            sys.exit(1)
+        api_communication.post_survey(api_token, survey_json_file)
+        api_communication.post_survey_invitations(api_token, api_communication.survey_id, email_list_file)
+    
+    except api_communication.APIConnectionException as e:
+        print(e)
+        sys.exit(1)
 
 
 # run main if main module is in use
